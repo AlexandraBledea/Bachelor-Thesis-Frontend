92df0e244936c4f4d9f24c250a3939c90b8d5986
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2023-06-15 16:53:09 +0300

    c

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -6,7 +6,7 @@ import {UserRegisterData} from "../shared/data-type/UserRegisterData";
 import {UserChangePasswordData} from "../shared/data-type/UserChangePasswordData";
 import {Recording} from "../shared/data-type/Recording";
 
-const URL_BASE = "http://20.223.159.18:8080/"
+const URL_BASE = "https://127.0.0.1:5000/"
 
 
 const LOGIN = URL_BASE + "login"
